5c96d95b55c6158b764df827e253a93a45f006a7
dcher <cernookijdima7@gmail.com>
2023-11-05 23:27:49 +0300

    debug

--- src/test/java/edu/project2Test/Project2MainTest.java
+++ src/test/java/edu/project2Test/Project2MainTest.java
@@ -4,6 +4,7 @@ import edu.project2.FirstGenerator;
 import edu.project2.FirstSolverDFS;
 import edu.project2.Maze;
 import edu.project2.MazeDrawer;
+import edu.project2.PrintMaze;
 import org.junit.jupiter.api.Test;
 
 public class Project2MainTest {
@@ -11,12 +12,13 @@ public class Project2MainTest {
 
     @Test
     public void project2MainTest() {
-        FirstGenerator generator = new FirstGenerator(70, 70);
+        FirstGenerator generator = new FirstGenerator(20, 20);
         Maze maze = generator.generateMaze();
         if(!FirstSolverDFS.solve(maze, maze.getRandomPassageCell(), maze.getRandomPassageCell())) {
             System.out.println("No path");
         }
-        MazeDrawer drawer2 = new MazeDrawer(maze, 10, 100);
+        MazeDrawer drawer2 = new MazeDrawer(maze, 20, 0);
+        PrintMaze.render(maze);
         drawer2.setVisible(true);
     }
 
