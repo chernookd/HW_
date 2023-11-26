0ed7337c31738e9fc6b73fffd69d5c7ab89ee322
dcher <cernookijdima7@gmail.com>
2023-11-26 19:12:57 +0300

    hw7

--- src/main/java/edu/hw7/Task1/IncThread.java
+++
@@ -1,9 +0,0 @@
-package edu.hw7.Task1;
-
-public class IncThread extends Thread {
-
-    @Override
-    public void run() {
-        Counter.counter.addAndGet(1);
-    }
-}
--- src/main/java/edu/hw7/Task3_5/Person.java
+++
@@ -1,3 +0,0 @@
-package edu.hw7.Task3_5;
-
-public record Person(int id, String name, String address, String phoneNumber) {}
